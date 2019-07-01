6d5dac3fa916e0e7de775e2bcb3707b51dcf3fcc
Amanda Hinchey <akhinchey@gmail.com>
2019-07-01 16:58:45 -0400

    Get tests working for authentication.

--- authentication.js
+++ authentication.js
@@ -1,7 +1,7 @@
 const getAccessToken = (z, bundle) => {
   const promise = z.request(`${process.env.BASE_URL}/oauth/access-token`, {
     method: 'POST',
-    params: {
+    body: {
       // accountDomain: bundle.cleanedRequest.querystring.accountDomain,
       code: bundle.inputData.code,
       client_id: process.env.CLIENT_ID,
@@ -62,7 +62,7 @@ const refreshAccessToken = (z, bundle) => {
 const testAuth = (z, bundle) => {
   // Normally you want to make a request to an endpoint that is either specifically designed to test auth, or one that
   // every user will have access to, such as an account or profile endpoint like /me.
-  const promise = z.request(`${process.env.BASE_URL}/api/v1/zapier_resources/me`, {
+  const promise = z.request(`${process.env.BASE_URL}/me`, {
     method: 'GET',
     params: {
       access_token: bundle.authData.access_token
